0b8a34ff964f9f65a4132794296260ac73cd8f37
blacklotus45 <s.molet@umiami.edu>
2016-02-03 14:35:34 -0500

    New Shape

--- shape/sketch.js
+++ shape/sketch.js
@@ -1,5 +1,6 @@
 var a;
 var b;
+var c;
 var y = 700;
 
 function setup() {
@@ -10,8 +11,16 @@ function draw() {
   console.log("X: " + mouseX, "Y:" + mouseY);
   var a = random(800);
   var b = random(0,600);
-  fill(0,0,0);
+
+  fill(0,0,0,10);
   rect(0,0,800,600);
+  
+  
+    fill(255,255,255);
+  if (mouseX < width) {
+    ellipse(mouseX, mouseY, 5, 5);
+  }
+  
   fill(255,255,255);
   ellipse(a,b,5,5);
   
@@ -22,10 +31,21 @@ function draw() {
   ellipse(420,560,10,10);
   
   triangle(250, 303, 404, 142, 552, 303);
-  
-  fill(255,255,200);
+   fill(255,255,205);
+  //window
+  if(mouseX >= 357 && mouseX <= 357 + 98) {
+    if(mouseY >= 352 && mouseY <= 352 + 98) {
+  fill(117,102,102);     
+    }
+  }
+
   rect(355,350,100,100);
   
+  //moon
+  if(y < 90)
+  {
+    y = 90
+  }
   fill(255,255,200);
   ellipse(85,y,120,120);
   ellipse(110,y + 20,10,10)
